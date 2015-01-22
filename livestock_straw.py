226aab7d372efea8326785b506305c75f95ff3b1
Freddy Banquez <freddy@localhost.localdomain>
2015-01-21 20:57:40 -0430

    se agregaron las descripciones emergentes a los campos en las ventanas de embriones y pajilla

--- livestock_straw.py
+++ livestock_straw.py
@@ -13,12 +13,12 @@ class livestock_straw(models.Model):
                ('mi', "Mini"))
 
     # Fields of the Straw Model
-    name = fields.Char(string='Name', size=25, required=True, select=True)
-    key =  fields.Char(string='Identifier', size=8, required=True)
-    color = fields.Char(string='Color', size=15, required=True)
-    types = fields.Selection(string='Type', selection=_type_straw_selection, required=True)
-    created_date = fields.Date(string='Created', default=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), required=True)
-    contents = fields.Char(string='Contents', size=25, required=True)
-    responsible = fields.Char(string='Responsible', size=25, required=True)
-    active = fields.Boolean(string='Active', default=True)
+    name = fields.Char(string='Name', size=25, required=True, select=True, help="Straw name")
+    key =  fields.Char(string='Identifier', size=8, required=True, help="Identifier of the straw")
+    color = fields.Char(string='Color', size=15, required=True, help="Color of the straw")
+    types = fields.Selection(string='Type', selection=_type_straw_selection, required=True, help="Typo of the straw")
+    created_date = fields.Date(string='Created', default=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), required=True, help="Creation date of the straw")
+    contents = fields.Char(string='Contents', size=25, required=True, help="Content of a straw")
+    responsible = fields.Char(string='Responsible', size=25, required=True, help="Biologist, veterinarian or person responsible for the creation of the straw")
+    active = fields.Boolean(string='Active', default=True, help="Enable/Disable record")
 
