119298e2a72b3a598222d2a989370acdf9c29be2
Freddy Banquez <freddy@localhost.localdomain>
2015-01-23 11:06:02 -0430

    se realizaron modificaciones a los valores por default de los campos created_date y responsible

--- livestock_straw.py
+++ livestock_straw.py
@@ -17,8 +17,8 @@ class livestock_straw(models.Model):
     key =  fields.Char(string='Identifier', size=8, required=True, help="Identifier of the straw")
     color = fields.Char(string='Color', size=15, required=True, help="Color of the straw")
     types = fields.Selection(string='Type', selection=_type_straw_selection, required=True, help="Typo of the straw")
-    created_date = fields.Date(string='Created', default=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), required=True, help="Creation date of the straw")
+    created_date = fields.Date(string='Created', default=datetime.now(), required=True, help="Creation date of the straw")
     contents = fields.Char(string='Contents', size=25, required=True, help="Content of a straw")
-    responsible = fields.Char(string='Responsible', size=25, required=True, help="Biologist, veterinarian or person responsible for the creation of the straw")
+    responsible = fields.Char(string='Responsible', size=25, required=True, default=lambda self: self.env.user.name, help="Biologist, veterinarian or person responsible for the creation of the straw")
     active = fields.Boolean(string='Active', default=True, help="Enable/Disable record")
 
