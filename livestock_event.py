324769eed7daf854154dcfd4f06bacf13a24c2b9
Freddy Banquez <freddy@localhost.localdomain>
2015-02-26 21:41:34 -0430

    Modificaciones hechas en el modelo evento para colocar la referencia hacia el animal.

--- livestock_event.py
+++ livestock_event.py
@@ -19,6 +19,6 @@ class livestock_event(models.Model):
     description = fields.Text(string='Description', required=True, help="Long description of the event")
     created_date = fields.Datetime(string='Date', required=True, default=datetime.now(), help="Date on which the event occurred")
     responsible = fields.Char(string='Responsible', required=True, size=30, default=lambda self: self.env.user.name, help="Person who reported the event")
-    #animal_id = fields.Many2one('livestock.animal', string='Animal', ondelete='cascade', index=True)
+    animal_id = fields.Many2one('livestock.animal', string='Animal', ondelete='cascade', index=True)
     active = fields.Boolean(string='Active', default=True, help="Enable/Disable event record")
 
