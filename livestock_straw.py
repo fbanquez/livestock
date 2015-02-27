cb48066bf2ff56158c69d2e848075e9b1533e9a2
Freddy Banquez <freddy@localhost.localdomain>
2015-02-26 21:25:09 -0430

    Modificaciones hechas en el modelo pajilla para colocar campos como seleccionables.

--- livestock_straw.py
+++ livestock_straw.py
@@ -12,13 +12,17 @@ class livestock_straw(models.Model):
         return(('nornal', "Normal"),
                ('mini', "Mini"))
 
+    def _contents_straw_selection(self):
+        return(('semen', 'Semen'),
+                ('embryo', 'Embryo'))
+
     # Fields of the Straw Model
     name = fields.Char(string='Identifier', size=8, required=True, help="Identifier of the straw")
     appellation =  fields.Char(string='Name', size=25, required=True, select=True, help="Straw name")
     color = fields.Char(string='Color', size=15, required=True, help="Color of the straw")
     types = fields.Selection(string='Type', selection=_type_straw_selection, required=True, help="Typo of the straw")
     created_date = fields.Date(string='Created', default=datetime.now(), required=True, help="Creation date of the straw")
-    contents = fields.Char(string='Contents', size=25, required=True, help="Content of a straw")
+    contents = fields.Selection(string='Contents', selection=_contents_straw_selection, required=True, help="Content of a straw")
     responsible = fields.Char(string='Responsible', size=25, required=True, default=lambda self: self.env.user.name, help="Biologist, veterinarian or person responsible for the creation of the straw")
     thermo_id = fields.Many2one('livestock.thermo', string='Thermo', ondelete='cascade', index=True)
     embryo_id = fields.Many2one('livestock.embryo', string='Embryo', ondelete='cascade', index=True)
